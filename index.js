edcc33c1c7171eae40291590b3026068ec0d5a7b
Mihai Plasoianu <mihai@plasoianu.de>
2020-12-07 19:31:17 +0100

    Fix variables

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const verifyChart = require('./lib/verify');
 const prepareChart = require('./lib/prepare');
 const publishChart = require('./lib/publish');
 
-let verified = true;
-let prepared = true;
+let verified = false;
+let prepared = false;
 
 async function verify(pluginConfig, context) {
     await verifyChart(pluginConfig, context);
